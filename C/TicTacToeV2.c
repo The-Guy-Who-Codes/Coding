254fa00111558a6bff23494052d634344a8494e3
Alexander Skaane <alexander.g.skaane@gmail.com>
2021-06-04 13:48:18 +0100

    commit

--- C/TicTacToeV2.c
+++ C/TicTacToeV2.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #define bool int
 
+// x and y coordinated have been flipped in their representation in arrays i.e [y, x] instead of [x, y], too lazy to fix (due to how cells 2D array is set up)
+
+const int BoardX = 3;
+const int BoardY = 3;
 char cell1[] = "|         ", cell2[] = "|         ", cell3[] = "|         |", cell4[] = "|         ", cell5[] = "|         ", cell6[] = "|         |", cell7[] = "|         ", cell8[] = "|         ", cell9[] = "|         |";
 const char spacer[] = "|         |         |         |";
 const char line[] = "|---------|---------|---------|";
@@ -14,10 +18,10 @@ void Input(char player) {
     bool accept = 0;
     do {
         printf("Player %c, what is the x value of the board where you want to place your token: ", player);
-        scanf("%i", &x);
-        printf("Player %c, what is the y value of the board where you want to place your token: ", player);
         scanf("%i", &y);
-        if (x > 2 || y > 2 || x < 0 || y < 0) {
+        printf("Player %c, what is the y value of the board where you want to place your token: ", player);
+        scanf("%i", &x);
+        if (x >= BoardX || y >= BoardY || x < 0 || y < 0) {
             printf("Coordinates out of range, try again!\n");
         } else {
             if (*cells[x][y] != ' ') {
@@ -33,11 +37,90 @@ void Input(char player) {
 
 }
 
+int ThreeInARow(char player) {
+    // gets all the coordinates of the player tokens
+    int tokens[6][2] = {{-1, -1}, {-1, -1}, {-1, -1}, {-1, -1}, {-1, -1}, {-1, -1}};
+    int tokenIter = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (*cells[i][j] == player) {
+                tokens[tokenIter][0] = i;
+                tokens[tokenIter][1] = j;
+                tokenIter++;
+            }
+        }
+    }
+    for (int z = 0; z < 7; z++) {
+        printf("%i, %i\n", tokens[z][0], tokens[z][1]);
+    }
+    // test for 3 in a row in a column
+    int XGroup[3][3] = {{-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}};
+    int X0, X1, X2;
+    X0 = X1 = X2 = 0;
+    for (int i = 0; i < 6; i++) {
+        // groups player tokens based on similar x coordinates
+        switch (tokens[i][1]) {
+            case 0:
+                XGroup[0][X0] = tokens[i][1];
+                X0++;
+                break;
+            case 1:
+                XGroup[1][X1] = tokens[i][1];
+                X1++;
+                break;
+            case 2:
+                XGroup[2][X2] = tokens[i][1];
+                X2++;
+                break;
+            default:
+                continue;
+        }
+    }
+    if (XGroup[0][2] != -1 || XGroup[1][2] != -1 || XGroup[2][2] != -1) {
+        printf(" PLayer %c Wins!!! Column!!!\n", player);
+        return 1;
+    }
+
+    // test for 3 in a row in a row
+    int YGroup[3][3] = {{-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}};
+    int Y0, Y1, Y2;
+    Y0 = Y1 = Y2 = 0;
+    for (int i = 0; i < 6; i++) {
+        // groups all player tokens based on their y coordinates
+        switch (tokens[i][0]) {
+            case 0:
+                YGroup[0][Y0] = tokens[i][0];
+                Y0++;
+                break;
+            case 1:
+                YGroup[1][Y1] = tokens[i][0];
+                Y1++;
+                break;
+            case 2:
+                YGroup[2][Y2] = tokens[i][0];
+                Y2++;
+                break;
+            default:
+                continue;
+        }
+    }
+    if (YGroup[0][2] != -1 || YGroup[1][2] != -1 || YGroup[2][2] != -1) {
+        printf("Player %c Wins!!! Row!!!\n", player);
+        return 1;
+    }
+
+    // test for 3 in a row diagonally
+
+
+}
+
 
 int main() {
-    while (1) {
+    bool endgame = 0;
+    while (endgame ==  0) {
         PrintBoard();
         Input('Z');
+        ThreeInARow('Z');
     }
     return 0;
 }
