66f45b88da7df4b7e280e17c6b137baf319b6c03
Alexander Skaane <alexander.g.skaane@gmail.com>
2021-06-12 21:55:19 +0100

    commit

--- C/sqrt.h
+++ C/sqrt.h
@@ -1,5 +1,15 @@
 
 
+double power(double value, int power){
+    double sum = value;
+    for (int i = 1; i < power; i++) {
+        sum *= value;
+    }
+    return sum;
+}
+
+
+
 double mySQRT(int value) {
     double x0 = value / 2;
     double x1;
@@ -13,6 +23,27 @@ double mySQRT(int value) {
 
     for (int i = 0; i < 4; i++) {
         x1 = x0 - (((x0 * x0) - value) / (2 * x0));
+        x0 = x1;
+    }
+    return x1;
+}
+
+
+
+
+
+double root(double value, double exponent) {
+    double x0, x1;
+
+    for (int i = 0; i < value / exponent; i++) {
+        if (power(i, exponent) < value && power((i + 1), exponent) > value) {
+            x0 = i;
+            break;
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        x1 = x0 - ((power(x0, exponent) - value) / (exponent * power(x0, exponent - 1)));
         x0 = x1;
     }
     return x1;
--- C/test.c
+++ C/test.c
@@ -3,6 +3,7 @@
 #include "sqrt.h"
 
 int main() {
-    printf("%.10f", mySQRT(500));
+    printf("%.10f\n", mySQRT(2));
+    printf("%.10f", root(8, 3));
     return 0;
 }
