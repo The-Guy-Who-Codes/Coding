5e50f2145132280c50a8c51e70ae6b1afbb51f77
Alexander Skaane <alexander.g.skaane.@gmail.com>
2021-06-13 21:03:21 +0100

    commit

--- C/sqrt.h
+++ C/sqrt.h
@@ -1,8 +1,8 @@
 
 
 double power(double value, int power){
-    double sum = value;
-    for (int i = 1; i < power; i++) {
+    double sum = 1;
+    for (int i = 0; i < power; i++) {
         sum *= value;
     }
     return sum;
@@ -11,16 +11,18 @@ double power(double value, int power){
 
 
 double mySQRT(int value) {
-    double x0 = value / 2;
-    double x1;
-
-    for (int i = 0; i < value / 2; i++) {
-        if (i * i < value && (i + 1) * (i + 1) > value) {
+    double x0, x1;
+    for (int i = 0; i < value; i++) {
+        
+        int t1 = power(i, 2);
+        int t2 = power(i + 1, 2);
+        
+        if (t1 <= value && t2 > value) {
             x0 = i;
             break;
         }
     }
-
+    
     for (int i = 0; i < 4; i++) {
         x1 = x0 - (((x0 * x0) - value) / (2 * x0));
         x0 = x1;
@@ -35,12 +37,17 @@ double mySQRT(int value) {
 double root(double value, double exponent) {
     double x0, x1;
 
-    for (int i = 0; i < value / exponent; i++) {
-        if (power(i, exponent) < value && power((i + 1), exponent) > value) {
+    for (int i = 0; i < value; i++) {
+        int t1 = power(i, exponent);
+        int t2 = power(i + 1, exponent);
+        // printf("DEBUG: %d\n%d\n", t1, t2);
+        if (t1 <= value && t2 > value) {
             x0 = i;
             break;
         }
     }
+    //x0 = value / exponent;
+    // printf("test: %f\n", x0);
 
     for (int i = 0; i < 4; i++) {
         x1 = x0 - ((power(x0, exponent) - value) / (exponent * power(x0, exponent - 1)));
--- C/test.c
+++ C/test.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include <stdint.h>
 #include "sqrt.h"
 
 int main() {
     printf("%.10f\n", mySQRT(2));
-    printf("%.10f", root(8, 3));
+    printf("%.10f", root(2749, 7));
     return 0;
 }
