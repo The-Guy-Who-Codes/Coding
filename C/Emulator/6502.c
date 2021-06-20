425993bd9ed78505edd8bf810d6bb04c1bc6c73b
Alexander Skaane <alexander.g.skaane.@gmail.com>
2021-06-20 22:06:42 +0100

    commit

--- C/Emulator/6502.c
+++ C/Emulator/6502.c
@@ -1,8 +1,58 @@
 #include "6502.h"
 
+void memInit(struct Mem mem) { // initialises memory by resetting all memory values to 0
+    for (int i = 0; i < MaxMem; i++) {
+        mem.Data[i] = 0;
+    }
+}
+
+void reset(struct CPU cpu, struct Mem mem) { // resets the state of the CPu as it were to be booted
+    cpu.PC = 0xFFFC; // the reset vector in memory for the 6502 [https://www.c64-wiki.com/wiki/Reset_(Process)] 
+    cpu.SP = 0x0100; 
+    cpu.D = 0; // clear decimal flag
+    cpu.C = cpu.Z = cpu.I = cpu.B = cpu.O = cpu.N = 0; // clear all flag registers
+    memInit(mem);
+}
+
+Byte fetch(uint32_t* cycles, struct Mem mem, struct CPU cpu) {
+    Byte Data = mem.Data[cpu.PC]; // fetches byte which PC is pointing to
+    printf("%x\n", Data);
+    cpu.PC++; // increment PC
+    *(cycles)--; // decriments cycle counter as fetching takes 1 cycle
+    return Data;
+}
+
+ void execute(uint32_t cycles, struct CPU cpu, struct Mem mem) {
+     while (cycles > 0) {
+        Byte Instruction = fetch(&cycles, mem, cpu);
+        switch (Instruction) {
+
+            // data from [http://www.obelisk.me.uk/6502/instructions.html]
+
+            case INS_LDA_IM: {
+                Byte Value = fetch(&cycles, mem, cpu);
+                cpu.A = Value;
+                cpu.Z = (cpu.A == 0); // set if A == 0
+                cpu.N = (cpu.A & 0b10000000) > 0; // set if Bit 7 of A is set
+            } break;
+
+
+            default: printf("[ERROR]: Instruction Not Handled: %d\n", Instruction); break;
+        }
+        cycles--;
+     }
+ }
+
+
 int main() {
     Mem mem;
     CPU cpu;
     reset(cpu, mem);
+    printf("%x\n", cpu.PC);
+    // test program
+    mem.Data[0xFFFC] = INS_LDA_IM;
+    mem.Data[0xFFFD] = 0x42;
+    // End Test
+    execute(2, cpu, mem);
     return 0;
 }
--- C/Emulator/6502.h
+++ C/Emulator/6502.h
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
-const int MaxMem = 1024 * 64; // defines the maximum amount of bits the 6502 can handle in memory
+#define MaxMem (1024 * 64) // defines the maximum amount of bytes the 6502 can handle in memory
+
+// opcodes
+
+#define INS_LDA_IM 0xA9
+
+
+
+
+typedef uint8_t Byte;
+typedef uint16_t Word;
 
 typedef struct Mem {
     uint8_t Data[MaxMem];
@@ -10,34 +20,19 @@ typedef struct Mem {
 typedef struct CPU {
     // info about registers [http://www.obelisk.me.uk/6502/registers.html]
 
-    uint16_t PC; // program counter
-    uint16_t SP; // stack pointer
+    Word PC; // program counter
+    Word SP; // stack pointer
 
-    uint8_t A, X, Y; // registers
+    Byte A, X, Y; // registers
 
     // status flags using bit fields
-    uint8_t C : 1; // carry
-    uint8_t Z : 1; // Zero
-    uint8_t I : 1; // interrupt Disable
-    uint8_t D : 1; // Decimal mode
-    uint8_t B : 1; // Break command
-    uint8_t O : 1; // Overflow
-    uint8_t N : 1; // negative
+    Byte C : 1; // carry
+    Byte Z : 1; // Zero
+    Byte I : 1; // interrupt Disable
+    Byte D : 1; // Decimal mode
+    Byte B : 1; // Break command
+    Byte O : 1; // Overflow
+    Byte N : 1; // negative
     
 
 } CPU;
-
-void memInit(struct Mem mem) {
-    for (int i = 0; i < MaxMem; i++) {
-        mem.Data[i] = 0;
-    }
-}
-
-void reset(struct CPU cpu, struct Mem mem) { // resets the state of the CPu as it were to be booted
-    cpu.PC = 0xFFFC; // the reset vector in memory for the 6502 [https://www.c64-wiki.com/wiki/Reset_(Process)] 
-    cpu.SP = 0x0100; 
-    cpu.D = 0; // clear decimal flag
-    cpu.C = cpu.Z = cpu.I = cpu.B = cpu.O = cpu.N = 0; // clear all flag registers
-    memInit(mem);
-
-}
