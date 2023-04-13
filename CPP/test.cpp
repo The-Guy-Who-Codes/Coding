4c7d98e5f4c0cd4a95593da6f8412f9d7826e1bb
Alexander Skaane <alexander@debian.debian11.com>
2023-04-13 23:47:50 +0200

    upadte

--- CPP/test.cpp
+++ CPP/test.cpp
@@ -1,7 +1,38 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
-#include <cmath>
+
+
+double pi = 3.141592654;
+
+void debugI(int a) {
+    printf("DEBUG: %i\n", a);
+}
+
+
+double pow(double a, int b) {
+    if (b == 0) {
+        return 1;
+    }
+    double tmp = a;
+    for (int i = 0; i < b - 1; i++) {
+        a *= tmp;
+    }
+    return a;
+}
+
+int factorial(int a) {
+    int sum = a;
+    for (int i = a - 1; i > 0; i--) {
+        sum *= i;
+    }
+    return sum;
+}
+
+double sin(double x) {
+    // sanitise input so that x is within +- pi
+    return x - (pow(x, 3) / 6) + (pow(x, 5 / 120)) - (pow(x, 7) / 5040) + (pow(x, 9) / 362880) - (pow(x, 11) / 39916800) + (pow(x, 13) / 1932053504);
+}
 
 
 class Matrix {
@@ -20,12 +51,12 @@ class Matrix {
                     int inp;
                     std::vector<int> inps;
                     inp = getchar();
-                    while(inp != 10){
+                    while (inp != 10){
                         inps.push_back(inp - 48);
                         inp = getchar();
                     };
                     int len = inps.size();
-                    for(int i = 0; i < len; i++) {
+                    for (int i = 0; i < len; i++) {
                         num += inps[i] * pow(10, len - (i + 1));
                     }
                     return num;
@@ -34,7 +65,7 @@ class Matrix {
         void init() {
             printf("Test\n[%i, %i]\n", w, h);
 
-            for(int i = 0; i < (w * h); i++) {
+            for (int i = 0; i < (w * h); i++) {
                 printf("Enter Value %i:\n", i + 1);
                 pmatrix[i] = inp_int();
 
@@ -43,15 +74,28 @@ class Matrix {
         }
 
         void printMat() {
-            for(int i = 0; i < h; i++) {
-                for(int x = 0; x < w; x++) {
+            for (int i = 0; i < h; i++) {
+                for (int x = 0; x < w; x++) {
                     printf(" %i ", pmatrix[i * w + x]);
                 }
                 printf("\n");
             }
+            printf("\n");
+
+        }
+
+        void scalar(int k) {
+            for (int i = 0; i < h; i++) {
+                for (int x = 0; x < w; x++) {
+                    pmatrix[i * w + x] *= k;
+                }
+            }
         }
 
 };
+
+
+
 
 Matrix::Matrix (int a, int b) {
     w = a;
@@ -60,9 +104,14 @@ Matrix::Matrix (int a, int b) {
 
 
 int main() {
-    Matrix mat (2, 2);
-    mat.init();
-    mat.printMat();
-    //printf("%i\n", (inp_int() - 5));
+    //Matrix mat (2, 2);
+    //mat.init();
+    //mat.printMat();
+   // mat.scalar(2);
+    //mat.printMat();
+    printf("%f\n", pow(pi / 2, 13));
+    printf("%f\n", sin(pi / 2));
+    
+    
     return 0;
 }
