c98e13d38dbcf6460d28c4da8b542926f413a511
= <junk.ags299@gmail.com>
2023-04-18 08:28:47 +0200

    update

--- CPP/Mathematica_knockoff/parser.hpp
+++ CPP/Mathematica_knockoff/parser.hpp
@@ -3,10 +3,14 @@
 #include <string>
 #include <cstdio>
 
+std::vector<std::string> func_tokens = {"pow", "sqrt", "root", "sin", "cos", "tan"};
+std::vector<int> func_num_params = {2, 1, 2, 1, 1, 1};
+
 void error(const char* msg) {
     int i = 0;
     while (msg[i] != '\0') {
         printf("%c", msg[i]);
     }
 }
+
 
